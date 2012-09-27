2acc320ca94e933898c805f825cc8aca38da490b
Norcross <andrew@andrewnorcross.com>
2012-09-27 15:08:53 -0400

    initial commit

---
+++ readme.txt
@@ -0,0 +1,48 @@
+=== WP Responsive Preview ===
+Contributors: norcross
+Website Link: http://andrewnorcross.com/plugins/yourls-link-creator/
+Donate link: https://andrewnorcross.com/donate
+Tags: responsive web design, rwd, browser previews
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Displays a post in a new window in various browser sizes.
+
+== Description ==
+
+Displays a post in a new window in various browser sizes.
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `wp-responsive-preview` to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Navigate to a post or page and click the "Responsive Preview" button
+
+== Frequently Asked Questions ==
+
+
+= Foo? =
+
+Bar
+
+
+== Screenshots ==
+
+1. Example of a post with a created link.
+
+
+== Changelog ==
+
+= 1.0 =
+* First release!
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+* First release!
---
+++ wp-responsive-preview.php
@@ -0,0 +1,118 @@
+<?php 
+/* 
+Plugin Name: WP Responsive Preview
+Plugin URI: http://andrewnorcross.com/plugins/
+Description: Displays a post in a new window in various browser sizes.
+Version: 1.0
+Author: Andrew Norcross
+Author URI: http://andrewnorcross.com
+
+    Copyright 2012 Andrew Norcross
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/ 
+
+
+
+// Start up the engine 
+class ResponsivePreview
+{
+	/**
+	 * Static property to hold our singleton instance
+	 * @var ResponsivePreview
+	 */
+	static $instance = false;
+
+
+	/**
+	 * This is our constructor, which is private to force the use of
+	 * getInstance() to make this a Singleton
+	 *
+	 * @return ResponsivePreview
+	 */
+	private function __construct() {
+		add_action		( 'admin_head', 					array( $this, 'css_head'		) );
+		add_action		( 'post_submitbox_misc_actions',	array( $this, 'rwd_button'		) );
+	}
+
+	/**
+	 * If an instance exists, this returns it.  If not, it creates one and
+	 * retuns it.
+	 *
+	 * @return ResponsivePreview
+	 */
+	 
+	public static function getInstance() {
+		if ( !self::$instance )
+			self::$instance = new self;
+		return self::$instance;
+	}
+
+
+	/**
+	 * CSS in the head for the settings page
+	 *
+	 * @return YOURLSCreator
+	 */
+
+	public function css_head() { ?>
+		<style type="text/css">
+
+		p.rwd-button {
+			text-align: center;
+			margin: 0.4em 0;
+		}
+		
+		</style>
+
+	<?php }
+
+	
+	/**
+	 * Add responsive button to publish metabox
+	 *
+	 * @return ResponsivePreview
+	 */
+
+	public function rwd_button() {
+	
+		global $post;
+
+		// bail if auto-draft, since there's nothing to preview
+		if ('auto-draft' == $post->post_status)
+			return;
+
+		// build out box		        
+		$rwd_id		= $post->ID;
+		$rwd_nonce	= wp_create_nonce('post_preview_' . $rwd_id);
+		
+		$rwd_draft	= add_query_arg( 'preview', 'true', get_permalink($rwd_id) );
+		$rwd_publ	= add_query_arg( array( 'preview' => 'true', 'preview_id' => $rwd_id, 'preview_nonce' => $rwd_nonce ), get_permalink($rwd_id) );
+
+		$rwd_root	= 'http://www.responsinator.com/';
+		$rwd_view	= 'publish' == $post->post_status ? $rwd_root.'?url='.esc_url($rwd_publ).'' : $rwd_root.'?url='.esc_url($rwd_draft);
+		
+		// display the button
+		echo '<div class="misc-pub-section misc-pub-section-last" style="border-top: 1px solid #eee;">';
+		echo '<p class="rwd-button"><a title="Click this button to load this content in a new window in various browser sizes." href="'.$rwd_view.'" class="button-secondary" target="_blank">View in Responsinator</a></p>';
+		echo '</div>';
+
+	}
+
+/// end class
+}
+
+
+// Instantiate our class
+$ResponsivePreview = ResponsivePreview::getInstance();
